3c0e41ec0b41a52b4eae48a1305f90000af5e858
Natalie Low <gnatann@gmail.com>
2019-03-07 10:13:10 -0800

    Chapter 1 complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "¡Hola, mundo! hello, world!"
+  end
+
+  def goodbye
+    render html: 'Goodbye'
+  end
+
 end
